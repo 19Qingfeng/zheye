845a032aa450c00b16da1c05a00d3ffa7e46d751
wang.haoyu <228233464@qq.com>
2021-03-05 11:27:22 +0800

    feat(init): develop

---
+++ front/vue-zheye/src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <golbal-header :user="userInfo" />
+    <column-list :list="list" />
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+import ColumnList, { ColumnListData } from './components/ColumnList.vue'
+import GolbalHeader, { UserProps } from './components/GolbalHeader.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const data: ColumnListData[] = [
+  {
+    id: 1,
+    title: 'hello',
+    avatar:
+      'https://th.bing.com/th/id/R5c74741127a42b3c550f0a6ecd82096a?rik=mSmOU8RfNwFUbQ&riu=http%3a%2f%2fimg17.3lian.com%2f201612%2f20%2f99b8790a4faae5fcebbe838eb24213ac.jpg&ehk=tGjAhjyRMAnzuFL%2b0y5ODF%2bozs8ahlOJ8%2fXRhxAri7E%3d&risl=&pid=ImgRaw',
+    description: '这是描述'
+  },
+  {
+    id: 2,
+    title: 'hello',
+    avatar: '',
+    description: '这是描述'
+  },
+  {
+    id: 3,
+    title: 'hello',
+    avatar:
+      'https://th.bing.com/th/id/R5c74741127a42b3c550f0a6ecd82096a?rik=mSmOU8RfNwFUbQ&riu=http%3a%2f%2fimg17.3lian.com%2f201612%2f20%2f99b8790a4faae5fcebbe838eb24213ac.jpg&ehk=tGjAhjyRMAnzuFL%2b0y5ODF%2bozs8ahlOJ8%2fXRhxAri7E%3d&risl=&pid=ImgRaw',
+    description: '这是描述'
+  },
+  {
+    id: 4,
+    title: 'hello',
+    avatar:
+      'https://th.bing.com/th/id/R5c74741127a42b3c550f0a6ecd82096a?rik=mSmOU8RfNwFUbQ&riu=http%3a%2f%2fimg17.3lian.com%2f201612%2f20%2f99b8790a4faae5fcebbe838eb24213ac.jpg&ehk=tGjAhjyRMAnzuFL%2b0y5ODF%2bozs8ahlOJ8%2fXRhxAri7E%3d&risl=&pid=ImgRaw',
+    description: '这是描述'
+  },
+  {
+    id: 5,
+    title: 'hello',
+    avatar:
+      'https://th.bing.com/th/id/R5c74741127a42b3c550f0a6ecd82096a?rik=mSmOU8RfNwFUbQ&riu=http%3a%2f%2fimg17.3lian.com%2f201612%2f20%2f99b8790a4faae5fcebbe838eb24213ac.jpg&ehk=tGjAhjyRMAnzuFL%2b0y5ODF%2bozs8ahlOJ8%2fXRhxAri7E%3d&risl=&pid=ImgRaw',
+    description: '这是描述'
+  },
+  {
+    id: 6,
+    title: 'hello',
+    avatar:
+      'https://th.bing.com/th/id/R5c74741127a42b3c550f0a6ecd82096a?rik=mSmOU8RfNwFUbQ&riu=http%3a%2f%2fimg17.3lian.com%2f201612%2f20%2f99b8790a4faae5fcebbe838eb24213ac.jpg&ehk=tGjAhjyRMAnzuFL%2b0y5ODF%2bozs8ahlOJ8%2fXRhxAri7E%3d&risl=&pid=ImgRaw',
+    description: '这是描述'
+  }
+]
+
+const userInfo: UserProps = {
+  isLogin: true,
+  name: 'wang.haoyu',
+  id: 1
+}
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList,
+    GolbalHeader
+  },
+  setup () {
+    return {
+      list: data,
+      userInfo
+    }
+  }
+})
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/vue-zheye/src/components/GolbalHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary mb-4 px-4">
+    <a class="navbar-brand" href="#">者也专栏</a>
+    <ul class="list-inline mb-0">
+      <template v-if="!user.isLogin">
+        <li class="list-inline-item">
+          <a href="" class="btn btn-outline-light my-2">登陆</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="" class="btn btn-outline-light my-2">注册</a>
+        </li>
+      </template>
+      <template v-else>
+        <li class="list-inline-item">
+          <dropdown :title="`你好 ${user.name}`" />
+        </li>
+      </template>
+    </ul>
+  </nav>
+</template>
+<script lang='ts'>
+import { defineComponent, PropType } from 'vue'
+import Dropdown from './Dropdown.vue'
+
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+
+export default defineComponent({
+  name: 'GolbalHeader',
+  components: {
+    Dropdown
+  },
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+</style>
